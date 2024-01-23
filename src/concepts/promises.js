e2f2341d682f568094d09e2e4fe30876d8eebd5a
RB <87038824+MrMhirs@users.noreply.github.com>
2024-01-23 12:34:35 +0100

    callback hell

--- src/concepts/promises.js
+++ src/concepts/promises.js
@@ -13,11 +13,27 @@ export const promiseComponent = (element) => {
     element.innerHTML = `<h1>Error</h1> <h3>${error}</h3>`;
   };
 
+  const renderTwoHeroes = (hero1, hero2) => {
+    element.innerHTML = `
+    <h3>${hero1.name}</h3>
+    <h3>${hero2.name}</h3>
+    `;
+  };
+
   const id1 = "5d86371f25a058e5b1c8a65e";
+  const id2 = "5d86371f9f80b591f499df32";
+
   findHero(id1)
-    .then(renderHero)
-    .catch((error) => renderError(error));
+    .then((hero1) => {
+      findHero(id2)
+        .then((hero2) => {
+          renderTwoHeroes(hero1, hero2);
+        })
+        .catch(renderError);
+    })
+    .catch(renderError);
 };
+
 /**
  *
  * @param {String} id

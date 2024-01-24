7ad81721001ad913d97dbe109c68bde8ba34f4d5
RB <87038824+MrMhirs@users.noreply.github.com>
2024-01-24 11:14:49 +0100

    Ex Funciones generadoras

--- src/concepts/generators.js
+++ src/concepts/generators.js
@@ -3,14 +3,33 @@
  * @param {HTMLDivElement} element
  */
 export const generatorComponents = (element) => {
-  const myGenerator = myFirstGeneratorFunction();
-  console.log(myGenerator.next());
-  console.log(myGenerator.next());
-  console.log(myGenerator.next());
-  console.log(myGenerator.next());
-  console.log(myGenerator.next());
-  console.log(myGenerator.next());
+  // const myGenerator = myFirstGeneratorFunction();
+  // console.log(myGenerator.next());
+  // console.log(myGenerator.next());
+  // console.log(myGenerator.next());
+  // console.log(myGenerator.next());
+  // console.log(myGenerator.next());
+  // console.log(myGenerator.next());
+
+  const genId = idGenerator();
+
+  const button = document.createElement("button");
+  button.innerText = "Click me";
+  element.append(button);
+
+  const renderButton = () => {
+    const { value } = genId.next();
+    button.innerText = `Click ${value}`;
+  };
+  button.addEventListener("click", () => renderButton());
 };
+
+function* idGenerator() {
+  let currentId = 0;
+  while (true) {
+    yield `2023-${++currentId}`;
+  }
+}
 
 function* myFirstGeneratorFunction() {
   yield "Primer valor";
